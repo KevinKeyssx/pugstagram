31712eeb7cd61bc2a23afcad659cb313c060e2bf
Keyss <55340042+KevinKeyssx@users.noreply.github.com>
2023-04-05 21:11:02 -0400

    Initial commit | Initial config

---
+++ src/containers/App.svelte
@@ -0,0 +1,13 @@
+<style>
+    :global(body) {
+        margin              : 0;
+        padding             : 0;
+        font-family         : sans-serif;
+        background-color    : #676778;
+        color               : white;
+    }
+</style>
+
+<div class="main">
+    <h1>Hello Pugstagram</h1>
+</div>
