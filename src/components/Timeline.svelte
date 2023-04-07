fdb8ba92c19b463d2fc78a4575e372ec9167033c
Keyss <55340042+KevinKeyssx@users.noreply.github.com>
2023-04-07 14:23:10 -0400

    Add all components

---
+++ src/components/Timeline.svelte
@@ -0,0 +1,22 @@
+<script>
+	import Card from './Card.svelte';
+
+	export let posts = [];
+</script>
+
+<style>
+	.Timeline { padding: 4em 0 0 0 }
+</style>
+
+<div class="Timeline">
+	<div class="Timeline-container">
+
+		{#each posts as post}
+			<Card {...post}/>
+		{:else}
+			<h3>Cargando...</h3>
+			<Card />
+		{/each}
+
+	</div>
+</div>
--- src/containers/App.svelte
+++ src/containers/App.svelte
@@ -1,13 +1,49 @@
+<script>
+	// !Imports
+    import Header 	    from '../components/Header.svelte';
+    import Timeline     from '../components/Timeline.svelte';
+    import Sidebar      from '../components/Sidebar.svelte';
+	import Footer 	    from "../components/Footer.svelte";
+    import Main         from '../components/Main.svelte';
+    import { onMount }  from 'svelte';
+
+    let data = {};
+    const API  = 'https://kittygram-api.now.sh/';
+
+    onMount(async () => {
+        const response = await fetch( API );
+        data = await response.json();
+
+		console.log('***DATA*** ~', data);
+    });
+
+</script>
+
 <style>
-    :global(body) {
+
+	@import url(https://fonts.googleapis.com/css?family=Lato:300,400&display=swap);
+	@import url(https://fonts.googleapis.com/css?family=Pacifico:swap);
+
+	:global(body) {
+        background-color    : #fafafa;
+        color               : rgba(38, 38, 38, 0.7);
+        font-family         : "Lato",sans-serif;
         margin              : 0;
         padding             : 0;
-        font-family         : sans-serif;
-        background-color    : #676778;
-        color               : white;
     }
+
+	:global(h1, h2, h3) {
+		margin              : 0;
+        padding             : 0;
+	}
+
 </style>
 
-<div class="main">
-    <h1>Hello Pugstagram</h1>
-</div>
+<Header />
+
+<Main>
+    <Timeline posts={data.posts} />
+    <Sidebar {...data.user}/>
+
+</Main>
+<Footer />
